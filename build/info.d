95cf02e40626e0d155b9f77f189e8b0c74934524
taellinglin <82527149+taellinglin@users.noreply.github.com>
2021-07-05 12:47:35 -0500

    added rom

---
+++ build/info.d
@@ -0,0 +1,98 @@
+info.o: c:\butano\common\src\info.cpp c:\butano\common\include\info.h \
+ C:/butano/butano/include/bn_vector.h \
+ C:/butano/butano/include/bn_assert.h \
+ C:/butano/butano/include/bn_type_traits.h \
+ C:/butano/butano/include/bn_common.h \
+ c:\butano\butano\hw\include\bn_hw_common.h \
+ C:/butano/butano/include/bn_config_assert.h \
+ C:/butano/butano/include/bn_config_doxygen.h \
+ C:/butano/butano/include/bn_sstream.h \
+ C:/butano/butano/include/bn_fixed_fwd.h \
+ C:/butano/butano/include/bn_string_fwd.h \
+ C:/butano/butano/include/bn_istring_base.h \
+ C:/butano/butano/include/bn_algorithm.h \
+ C:/butano/butano/include/bn_functional.h \
+ C:/butano/butano/include/bn_utility.h \
+ C:/butano/butano/include/bn_compare.h \
+ C:/butano/butano/include/bn_iterator.h \
+ C:/butano/butano/include/bn_vector_fwd.h \
+ C:/butano/butano/include/bn_span_fwd.h \
+ C:/butano/butano/include/bn_sprite_ptr.h \
+ C:/butano/butano/include/bn_optional_fwd.h \
+ C:/butano/butano/include/bn_keypad.h \
+ C:/butano/butano/include/bn_display.h \
+ c:\butano\butano\hw\include\bn_hw_display_constants.h \
+ C:/butano/butano/include/bn_common.h \
+ C:/butano/butano/include/bn_sprite_text_generator.h \
+ C:/butano/butano/include/bn_fixed.h C:/butano/butano/include/bn_limits.h \
+ C:/butano/butano/include/bn_vector.h \
+ C:/butano/butano/include/bn_sprite_font.h \
+ C:/butano/butano/include/bn_string_view.h \
+ C:/butano/butano/include/bn_sprite_item.h \
+ C:/butano/butano/include/bn_sprite_shape_size.h \
+ C:/butano/butano/include/bn_bpp_mode.h \
+ C:/butano/butano/include/bn_sprite_tiles_item.h \
+ C:/butano/butano/include/bn_span.h C:/butano/butano/include/bn_array.h \
+ C:/butano/butano/include/bn_array_fwd.h \
+ C:/butano/butano/include/bn_span_fwd.h \
+ C:/butano/butano/include/bn_tile.h \
+ C:/butano/butano/include/bn_compression_type.h \
+ C:/butano/butano/include/bn_sprite_palette_item.h \
+ C:/butano/butano/include/bn_color.h \
+ C:/butano/butano/include/bn_alignment.h \
+ C:/butano/butano/include/bn_utf8_character.h \
+ C:/butano/butano/include/bn_config_sprite_text.h \
+ C:/butano/butano/include/bn_unordered_map.h \
+ C:/butano/butano/include/bn_memory.h \
+ C:/butano/butano/include/bn_power_of_two.h \
+ C:/butano/butano/include/bn_unordered_map_fwd.h
+c:\butano\common\include\info.h:
+C:/butano/butano/include/bn_vector.h:
+C:/butano/butano/include/bn_assert.h:
+C:/butano/butano/include/bn_type_traits.h:
+C:/butano/butano/include/bn_common.h:
+c:\butano\butano\hw\include\bn_hw_common.h:
+C:/butano/butano/include/bn_config_assert.h:
+C:/butano/butano/include/bn_config_doxygen.h:
+C:/butano/butano/include/bn_sstream.h:
+C:/butano/butano/include/bn_fixed_fwd.h:
+C:/butano/butano/include/bn_string_fwd.h:
+C:/butano/butano/include/bn_istring_base.h:
+C:/butano/butano/include/bn_algorithm.h:
+C:/butano/butano/include/bn_functional.h:
+C:/butano/butano/include/bn_utility.h:
+C:/butano/butano/include/bn_compare.h:
+C:/butano/butano/include/bn_iterator.h:
+C:/butano/butano/include/bn_vector_fwd.h:
+C:/butano/butano/include/bn_span_fwd.h:
+C:/butano/butano/include/bn_sprite_ptr.h:
+C:/butano/butano/include/bn_optional_fwd.h:
+C:/butano/butano/include/bn_keypad.h:
+C:/butano/butano/include/bn_display.h:
+c:\butano\butano\hw\include\bn_hw_display_constants.h:
+C:/butano/butano/include/bn_common.h:
+C:/butano/butano/include/bn_sprite_text_generator.h:
+C:/butano/butano/include/bn_fixed.h:
+C:/butano/butano/include/bn_limits.h:
+C:/butano/butano/include/bn_vector.h:
+C:/butano/butano/include/bn_sprite_font.h:
+C:/butano/butano/include/bn_string_view.h:
+C:/butano/butano/include/bn_sprite_item.h:
+C:/butano/butano/include/bn_sprite_shape_size.h:
+C:/butano/butano/include/bn_bpp_mode.h:
+C:/butano/butano/include/bn_sprite_tiles_item.h:
+C:/butano/butano/include/bn_span.h:
+C:/butano/butano/include/bn_array.h:
+C:/butano/butano/include/bn_array_fwd.h:
+C:/butano/butano/include/bn_span_fwd.h:
+C:/butano/butano/include/bn_tile.h:
+C:/butano/butano/include/bn_compression_type.h:
+C:/butano/butano/include/bn_sprite_palette_item.h:
+C:/butano/butano/include/bn_color.h:
+C:/butano/butano/include/bn_alignment.h:
+C:/butano/butano/include/bn_utf8_character.h:
+C:/butano/butano/include/bn_config_sprite_text.h:
+C:/butano/butano/include/bn_unordered_map.h:
+C:/butano/butano/include/bn_memory.h:
+C:/butano/butano/include/bn_power_of_two.h:
+C:/butano/butano/include/bn_unordered_map_fwd.h:
